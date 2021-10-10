81cb84254e0747b9b7fce2da4bac0b76389ffb12
JJLee-20194099 <JJLee-20194099>
2021-10-10 13:46:03 +0700

    Improve eat method from cat module

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ var cat = new Cat("Tom");
 var mouse = new Mouse("Jerry");
 var dog = new Dog("JJLee");
 cat.eat(mouse);
-cat.eat(dog);
+try {
+	cat.eat(dog);
+} catch(error) {
+	console.log('Error while cat eating animals that are not mouse!')
+}
